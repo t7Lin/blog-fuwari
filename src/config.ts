6d73d88befdbbe4309917e07bc5bc3fafbab8e9c
t7Lin <chilltian@outlook.com>
2025-10-02 11:56:22 +0800

    1002

--- src/config.ts
+++ src/config.ts
@@ -8,8 +8,8 @@ import type {
 import { LinkPreset } from "./types/config";
 
 export const siteConfig: SiteConfig = {
-	title: "Chillyt's blog",
-	subtitle: "若有似无的甜",
+	title: "Ember's blog",
+	subtitle: "炽热烈火燃烧，静默余烬温存",
 	lang: "en", // 'en', 'zh_CN', 'zh_TW', 'ja', 'ko', 'es', 'th'
 	themeColor: {
 		hue: 60, // Default hue for the theme color, from 0 to 360. e.g. red: 0, teal: 200, cyan: 250, pink: 345
@@ -62,7 +62,7 @@ export const navBarConfig: NavBarConfig = {
 
 export const profileConfig: ProfileConfig = {
 	avatar: "assets/images/avatar.png", // Relative to the /src directory. Relative to the /public directory if it starts with '/'
-	name: "Chillyt",
+	name: "Ember",
 	bio: "Adventure awaits!",
 	links: [
 		{
